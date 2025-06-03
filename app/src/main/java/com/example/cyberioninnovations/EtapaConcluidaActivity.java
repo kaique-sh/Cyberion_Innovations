3d80988cb6b1adc41e32727b1f37599c406effd3
kaique.silva <kaique.rodrigues328328a@gmail.com>
2025-06-03 17:38:40 -0300

    Parte do kaique

---
+++ app/src/main/java/com/example/cyberioninnovations/EtapaConcluidaActivity.java
@@ -0,0 +1,4 @@
+package com.example.cyberioninnovations;
+
+public class EtapaConcluidaActivity {
+}
---
+++ app/src/main/java/com/example/cyberioninnovations/SolicitarSuporteActivity.java
@@ -0,0 +1,50 @@
+package com.example.solicitarsuporte;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cyberioninnovations.EtapaConcluidaActivity;
+import com.example.cyberioninnovations.R;
+
+public class SolicitarSuporteActivity extends AppCompatActivity {
+
+    EditText edtAssunto, edtDescricao;
+    Spinner spinnerChamado;
+    Button btnEnviar;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solicitar_suporte);
+
+        edtAssunto = findViewById(R.id.edtAssunto);
+        edtDescricao = findViewById(R.id.edtDescricao);
+        spinnerChamado = findViewById(R.id.spinnerChamado);
+        btnEnviar = findViewById(R.id.btnEnviar);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String assunto = edtAssunto.getText().toString();
+                String descricao = edtDescricao.getText().toString();
+
+                if (assunto.isEmpty() || descricao.isEmpty()) {
+                    Toast.makeText(SolicitarSuporteActivity.this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(SolicitarSuporteActivity.this, EtapaConcluidaActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+}
